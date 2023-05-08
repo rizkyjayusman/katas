4edef7e1a75440138eb5630da8eb48d4fde5c081
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-05-08 16:17:04 +0700

    handle update item quality per item name

--- gilded-rose/gildedrose/gildedrose.go
+++ gilded-rose/gildedrose/gildedrose.go
@@ -12,49 +12,49 @@ func UpdateQuality(items []*Item) {
 }
 
 func UpdateItemQuality(item *Item) {
-	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+	if item.Name == "+5 Dexterity Vest" || item.Name == "Elixir of the Mongoose" {
 		if item.Quality > 0 {
-			if item.Name != "Sulfuras, Hand of Ragnaros" {
-				item.Quality = item.Quality - 1
-			}
+			item.Quality = item.Quality - 1
 		}
-	} else {
+
+		item.SellIn = item.SellIn - 1
+
+		if item.SellIn < 0 && item.Quality > 0 {
+			item.Quality = item.Quality - 1
+		}
+	} else if item.Name == "Aged Brie" {
 		if item.Quality < 50 {
 			item.Quality = item.Quality + 1
-			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-				if item.SellIn < 11 {
-					if item.Quality < 50 {
-						item.Quality = item.Quality + 1
-					}
-				}
-				if item.SellIn < 6 {
-					if item.Quality < 50 {
-						item.Quality = item.Quality + 1
-					}
-				}
-			}
 		}
-	}
 
-	if item.Name != "Sulfuras, Hand of Ragnaros" {
 		item.SellIn = item.SellIn - 1
-	}
 
-	if item.SellIn < 0 {
-		if item.Name != "Aged Brie" {
-			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
-				if item.Quality > 0 {
-					if item.Name != "Sulfuras, Hand of Ragnaros" {
-						item.Quality = item.Quality - 1
-					}
-				}
-			} else {
-				item.Quality = item.Quality - item.Quality
-			}
-		} else {
-			if item.Quality < 50 {
+		if item.SellIn < 0 && item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+	} else if item.Name == "Sulfuras, Hand of Ragnaros" {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+	} else if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		if item.Quality < 50 {
+			item.Quality = item.Quality + 1
+		}
+
+		if item.Quality < 50 {
+			if item.SellIn < 11 {
 				item.Quality = item.Quality + 1
 			}
+
+			if item.SellIn < 6 {
+				item.Quality = item.Quality + 1
+			}
+		}
+
+		item.SellIn = item.SellIn - 1
+
+		if item.SellIn < 0 {
+			item.Quality = item.Quality - item.Quality
 		}
 	}
 }
--- gilded-rose/gildedrose/gildedrose_test.go
+++ gilded-rose/gildedrose/gildedrose_test.go
@@ -19,6 +19,23 @@ func Test_Foo(t *testing.T) {
 }
 
 func TestPlusFiveDexterityVest1(t *testing.T) {
+	item := &gildedrose.Item{"+5 Dexterity Vest", -1, 20}
+	gildedrose.UpdateItemQuality(item)
+
+	expected := &gildedrose.Item{"+5 Dexterity Vest", -2, 18}
+	if item.Quality != expected.Quality {
+		t.Error("value of quality is not match")
+	}
+
+	if item.SellIn != expected.SellIn {
+		t.Error("value of sell in is not match")
+	}
+
+	// quality = -1
+	// sell in = -1
+}
+
+func TestPlusFiveDexterityVest2(t *testing.T) {
 	item := &gildedrose.Item{"+5 Dexterity Vest", 10, 20}
 	gildedrose.UpdateItemQuality(item)
 
@@ -40,6 +57,23 @@ func TestAgedBrie1(t *testing.T) {
 	gildedrose.UpdateItemQuality(item)
 
 	expected := &gildedrose.Item{"Aged Brie", 1, 1}
+	if item.Quality != expected.Quality {
+		t.Error("value of quality is not match")
+	}
+
+	if item.SellIn != expected.SellIn {
+		t.Error("value of sell in is not match")
+	}
+
+	// quality = -1
+	// sell in = +1
+}
+
+func TestAgedBrie2(t *testing.T) {
+	item := &gildedrose.Item{"Aged Brie", -1, 0}
+	gildedrose.UpdateItemQuality(item)
+
+	expected := &gildedrose.Item{"Aged Brie", -2, 2}
 	if item.Quality != expected.Quality {
 		t.Error("value of quality is not match")
 	}
@@ -86,11 +120,79 @@ func TestSulfurasHandOfRagnaros1(t *testing.T) {
 	// sell in = 0
 }
 
+func TestSulfurasHandOfRagnaros2(t *testing.T) {
+	item := &gildedrose.Item{"Sulfuras, Hand of Ragnaros", 0, 30}
+	gildedrose.UpdateItemQuality(item)
+
+	expected := &gildedrose.Item{"Sulfuras, Hand of Ragnaros", 0, 31}
+	if item.Quality != expected.Quality {
+		t.Error("value of quality is not match")
+	}
+
+	if item.SellIn != expected.SellIn {
+		t.Error("value of sell in is not match")
+	}
+
+	// quality = 0
+	// sell in = 0
+}
+
 func TestBackstagePassesToATAFKAL80ETCConcert(t *testing.T) {
 	item := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20}
 	gildedrose.UpdateItemQuality(item)
 
-	expected := &gildedrose.Item{"Sulfuras, Hand of Ragnaros", 14, 21}
+	expected := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", 14, 21}
+	if item.Quality != expected.Quality {
+		t.Error("value of quality is not match")
+	}
+
+	if item.SellIn != expected.SellIn {
+		t.Error("value of sell in is not match")
+	}
+
+	// quality = -1
+	// sell in = +1
+}
+
+func TestBackstagePassesToATAFKAL80ETCConcert2(t *testing.T) {
+	item := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", 6, 20}
+	gildedrose.UpdateItemQuality(item)
+
+	expected := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", 5, 22}
+	if item.Quality != expected.Quality {
+		t.Error("value of quality is not match")
+	}
+
+	if item.SellIn != expected.SellIn {
+		t.Error("value of sell in is not match")
+	}
+
+	// quality = -1
+	// sell in = +1
+}
+
+func TestBackstagePassesToATAFKAL80ETCConcert3(t *testing.T) {
+	item := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", 10, 20}
+	gildedrose.UpdateItemQuality(item)
+
+	expected := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", 9, 22}
+	if item.Quality != expected.Quality {
+		t.Error("value of quality is not match")
+	}
+
+	if item.SellIn != expected.SellIn {
+		t.Error("value of sell in is not match")
+	}
+
+	// quality = -1
+	// sell in = +1
+}
+
+func TestBackstagePassesToATAFKAL80ETCConcert4(t *testing.T) {
+	item := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", -1, 20}
+	gildedrose.UpdateItemQuality(item)
+
+	expected := &gildedrose.Item{"Backstage passes to a TAFKAL80ETC concert", -2, 0}
 	if item.Quality != expected.Quality {
 		t.Error("value of quality is not match")
 	}
